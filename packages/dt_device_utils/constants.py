648a75af1095dda83e2ad1fed6b4f03b62753c7e
Andrea F. Daniele <afdaniele@ttic.edu>
2020-10-08 02:25:40 -0500

    added packages: dt_device_utils, dt_permissions_utils, dt_secrets_utils

---
+++ packages/dt_device_utils/constants.py
@@ -0,0 +1,1 @@
+ETH0_DEVICE_MAC_FILE = "/data/stats/MAC/eth0"
---
+++ packages/dt_device_utils/device.py
@@ -0,0 +1,35 @@
+import os
+import socket
+import logging
+
+from .constants import ETH0_DEVICE_MAC_FILE
+
+# create logger
+logging.basicConfig()
+logger = logging.getLogger(os.environ.get('DT_MODULE_TYPE', 'module') + '.device')
+logger.setLevel(logging.INFO)
+if 'DEBUG' in os.environ and os.environ['DEBUG'].lower() in ['true', 'yes', '1']:
+    logger.setLevel(logging.DEBUG)
+
+
+def get_device_id() -> str:
+    if not os.path.isfile(ETH0_DEVICE_MAC_FILE):
+        msg = f"File '{ETH0_DEVICE_MAC_FILE}' not found. Cannot compute unique device ID."
+        logger.error(msg)
+        raise ValueError(msg)
+    # read MAC from file
+    with open(ETH0_DEVICE_MAC_FILE, 'rt') as fin:
+        mac = fin.read().strip()
+    # turn MAC into a unique ID
+    device_id = mac.replace(':', '').strip()
+    # make sure we have a valid MAC address
+    if len(device_id) != 12:
+        msg = f"Invalid MAC address '{mac}'. Cannot compute unique device ID."
+        logger.error(msg)
+        raise ValueError(msg)
+    # ---
+    return device_id
+
+
+def get_device_hostname() -> str:
+    return socket.gethostname()
