7b8339663d531475f56ce1a9fbaf3a52c7f46a82
Andrea F. Daniele <afdaniele@ttic.edu>
2020-10-29 23:57:32 -0500

    minor reformatting

--- assets/environment.sh
+++ assets/environment.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 dt-terminate() {
-  # send SIGINT signal to monitored process
-  kill -INT $(pgrep -P $$) 2> /dev/null
+    # send SIGINT signal to monitored process
+    kill -INT $(pgrep -P $$) 2>/dev/null
 }
 
 dt-register-signals() {
-  trap dt-terminate SIGINT
-  trap dt-terminate SIGTERM
+    trap dt-terminate SIGINT
+    trap dt-terminate SIGTERM
 }
 
 dt-init() {
-  # register signal handlers
-  dt-register-signals
+    # register signal handlers
+    dt-register-signals
 }
 
 dt-join() {
-  # wait for all the processes in the background to terminate
-  set +e
-  wait &> /dev/null
-  set -e
+    # wait for all the processes in the background to terminate
+    set +e
+    wait &>/dev/null
+    set -e
 }
 
 dt-launchfile-init() {
-  set -e
-  # register signal handlers
-  dt-register-signals
-  echo "=> Launching app..."
+    set -e
+    # register signal handlers
+    dt-register-signals
+    echo "=> Launching app..."
 }
 
 dt-launchfile-join() {
-  # wait for the process to end
-  dt-join
-  # wait for stdout to flush, then announce app termination
-  sleep 0.5
-  printf "<= App terminated!\n"
+    # wait for the process to end
+    dt-join
+    # wait for stdout to flush, then announce app termination
+    sleep 0.5
+    printf "<= App terminated!\n"
 }
 
 dt-exec() {
-  cmd="$@"
-  cmd="${cmd%&} &"
-  eval "${cmd}"
+    cmd="$@"
+    cmd="${cmd%&} &"
+    eval "${cmd}"
 }
